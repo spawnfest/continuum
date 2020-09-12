fee096aba3d934ec460427456daa2b80631fdff4
James Edward Gray II <james@graysoftinc.com>
2020-09-11 19:09:13 -0500

    mix new

---
+++ lib/continuum.ex
@@ -0,0 +1,18 @@
+defmodule Continuum do
+  @moduledoc """
+  Documentation for `Continuum`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Continuum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/continuum_test.exs
@@ -0,0 +1,8 @@
+defmodule ContinuumTest do
+  use ExUnit.Case
+  doctest Continuum
+
+  test "greets the world" do
+    assert Continuum.hello() == :world
+  end
+end
